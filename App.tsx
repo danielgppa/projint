05361c2d1e8f883da8d531c0c64d7f557d7c1b84
noteb <danielgppa@gmail.com>
2023-04-26 20:58:14 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,57 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Avatar, Button } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { IconButton } from 'react-native-paper';
+
+// Importe seus componentes HomeScreen e SettingsScreen ou crie-os temporariamente
+function HomeScreen() {
+  return (   
+    <>
+    <View style={styles.container}>
+    <View style={styles.avatarContainer}>
+      <Avatar.Image size={60} source={require('./assets/avatar.png')} />
+    </View>
+    <IconButton icon="medal" size={30} color="#000" style={{marginTop: 50, marginRight: 300}} />
+    <IconButton icon="trophy" size={30} color="#000" style={{marginTop: 50, marginRight: 300}} />
+    <IconButton icon="star" size={30} color="#000" style={{marginTop: 50, marginRight: 300}} />
+    <IconButton icon="snake" size={30} color="#000" style={{marginTop: 50, marginRight: 300}} />
+    </View>
+    <StatusBar style="dark" />
+    </>
+    
+  )
+
+}
+
+function SettingsScreen() {
+  return     <Text>Settings</Text>;
+
+  // <View id='settings'>
+    // </View>
+  
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Perfil" component={HomeScreen} />
+          <Tab.Screen name="Feed" component={SettingsScreen} />
+          <Tab.Screen name="Comunidade" component={SettingsScreen} />
+          <Tab.Screen name="Requerimento Web" component={SettingsScreen} />
+          <Tab.Screen name="Ajuda" component={SettingsScreen} />
+
+        </Tab.Navigator>
+      </NavigationContainer>
+    // </View>
   );
 }
 
@@ -15,6 +60,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  avatarContainer: {
+    marginTop: 50,
+    marginRight: 300,
   },
 });
